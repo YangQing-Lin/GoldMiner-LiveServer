31d41326949ec449493c8c06f593039ca01936b6
YangQing-Lin <1665486570@qq.com>
2022-06-12 13:44:28 +0800

    发布版本v-1.5

--- scripts/rsync_to_local_gold_miner.sh
+++ scripts/rsync_to_local_gold_miner.sh
@@ -3,6 +3,3 @@
 
 rsync -avzu --progress ../static/js/src/playground/ \
 /mnt/d/Project/AcWing/AcApp-GoldMiner/game/static/js/src/playground/
-
-rsync -avzu --progress ../README.md \
-/mnt/d/Project/AcWing/AcApp-GoldMiner/README.md
